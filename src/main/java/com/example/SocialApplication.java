c08fa469d797b5b7dc6105d51376cfc883cf6d04
Daniel Song <xdtsong@gmail.com>
2017-04-14 14:29:05 -0700

    add config to integrate Facebook login

--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-public class SsoLoginApplication {
+@EnableOAuth2Sso
+public class SocialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsoLoginApplication.class, args);
+		SpringApplication.run(SocialApplication.class, args);
 	}
 }
