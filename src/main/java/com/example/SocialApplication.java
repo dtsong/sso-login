84de4356a7f4e93b3afda1fe613bd6e758211e0b
Daniel Song <xdtsong@gmail.com>
2017-04-14 14:40:22 -0700

    add User endpoint to app

--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -3,10 +3,18 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
 public class SocialApplication {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
